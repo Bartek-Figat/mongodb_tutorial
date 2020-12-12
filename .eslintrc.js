1c8163546ae366e6ff832a67e0bf51013e5df3d0
Bartek-Figat <figat29@gmail.com>
2020-12-11 18:34:13 -0800

    new v1

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,19 @@ export default {
     browser: true,
     es2021: true,
   },
-  extends: "eslint:recommended",
+  extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+  },
+  plugins: ['prettier'],
 };
